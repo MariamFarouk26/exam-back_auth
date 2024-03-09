fd068d67b46dfdc6ed930a74a59fb91514e7f394
islam-farahat <islam.mostafa.f@gmail.com>
2024-03-09 02:23:22 +0200

    add cores

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 200,
+    },
+  });
   const config = new DocumentBuilder()
     .setTitle('Exams API')
     .setDescription('The exams API description')
