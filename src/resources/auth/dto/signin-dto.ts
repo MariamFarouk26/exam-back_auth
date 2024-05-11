ade474a1939fd9828c02a7b27c40ffc1f59e84ed
islam-farahat <islam.mostafa.f@gmail.com>
2024-05-11 20:41:23 +0300

    update

--- src/resources/auth/dto/signin-dto.ts
+++ src/resources/auth/dto/signin-dto.ts
@@ -1,14 +1,17 @@
 
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
 
 export class signinDto {
     @IsNotEmpty()
     @IsString()
     @IsEmail()
-    public email :string;
+    @ApiProperty()
+    public email: string;
 
     @IsNotEmpty()
     @IsString()
-    @Length(3,20,{ message: "pass must be between 3 to 20 char" } )
-    public password :string;
+    @Length(3, 20, { message: "pass must be between 3 to 20 char" })
+    @ApiProperty()
+    public password: string;
 }
