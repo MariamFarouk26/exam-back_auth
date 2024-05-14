1da23acdb5fd07e88f6ea6635b88d82012be769b
islam-farahat <islam.mostafa.f@gmail.com>
2024-05-14 21:51:47 +0300

    add token validation

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
-import {JwtService} from "@nestjs/jwt"
+import { JwtService } from "@nestjs/jwt"
 import { jwtSecret } from './utils/constants';
 import { Request, Response } from 'express';
 import { PrismaService } from 'src/db/prisma.service';
@@ -10,55 +10,55 @@ import { signinDto } from './dto/signin-dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma:PrismaService , private jwt:JwtService){}
+  constructor(private prisma: PrismaService, private jwt: JwtService) { }
 
 
-  async SignUp(dto:signupDto){
-    const{email , password,userName} = dto;
+  async SignUp(dto: signupDto) {
+    const { email, password, userName } = dto;
 
     const foundUser = await this.IsfoundUser(email);
-    if(foundUser) {
+    if (foundUser) {
       throw new BadRequestException('Email is taken')
     }
 
     const hashedpassword = await this.hashPassword(password);
     await this.prisma.user.create({
-      data:{
+      data: {
         email,
-        hashPass:hashedpassword,
+        hashPass: hashedpassword,
         userName
-      }});
-    
-    return {message: `signup successfully , ${userName}`}
+      }
+    });
+
+    return { message: `signup successfully , ${userName}` }
   }
 
 
-  async SignIn(dto:signinDto , req:Request , res:Response){
-    const{email , password} = dto;
+  async SignIn(dto: signinDto, req: Request, res: Response) {
+    const { email, password } = dto;
 
     //to check the email
     const foundUser = await this.IsfoundUser(email);
 
-    if(!foundUser) {
+    if (!foundUser) {
       console.log(foundUser)
       throw new BadRequestException('Wrong Email or Password')
     }
 
-     //to check the pass
-    const IsMatch = await this.comparePassword(password,foundUser.hashPass);
-    if(!IsMatch) {
+    //to check the pass
+    const IsMatch = await this.comparePassword(password, foundUser.hashPass);
+    if (!IsMatch) {
       console.log(IsMatch)
       throw new BadRequestException('Wrong Email or Password')
     }
 
     //sign jwt and return user
-    const token = await this.signToken({id: foundUser.id,email:foundUser.email})
-    if(!token) {
+    const token = await this.signToken({ id: foundUser.id, email: foundUser.email })
+    if (!token) {
       //stop func if no token else send jwt as cookie to client 
       throw new ForbiddenException()
-    } 
-  
-    res.cookie("Token",token)
+    }
+
 
     return res.send({
       message: 'logged in successfully',
@@ -68,30 +68,29 @@ export class AuthService {
 
 
   //clean token from client
-  async SignOut( res:Response){
+  async SignOut(res: Response) {
     //remove cookie by it's name which used in sign in
     res.clearCookie("Token")
-    return res.send({ message : "logged out successfully" })
+    return res.send({ message: "logged out successfully" })
   }
 
-  async hashPassword(password:string){
+  async hashPassword(password: string) {
     const saltOrRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltOrRounds);
     return hashedPassword
   }
 
-  async IsfoundUser(email:string){
-    const found = await this.prisma.user.findUnique({where:{email}})
+  async IsfoundUser(email: string) {
+    const found = await this.prisma.user.findUnique({ where: { email } })
     return found
   }
 
-  async comparePassword(password:string ,hash:string)
-  {
+  async comparePassword(password: string, hash: string) {
     return await bcrypt.compare(password, hash);
   }
 
-  async signToken (args:{ id : string ; email : string}){
+  async signToken(args: { id: string; email: string }) {
     let payload = args
-    return this.jwt.signAsync(payload,{secret:jwtSecret})
+    return this.jwt.signAsync(payload, { secret: jwtSecret })
   }
 }
--- src/resources/auth/jwt.strategy.ts
+++ src/resources/auth/jwt.strategy.ts
@@ -9,21 +9,22 @@ import { Request } from "express";
 export class jwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest:ExtractJwt.fromExtractors([jwtStrategy.extractJWT]),
-        secretOrKey:jwtSecret,
+      jwtFromRequest: ExtractJwt.fromExtractors([jwtStrategy.extractJWT]),
+      secretOrKey: jwtSecret,
     });
   }
 
-  private static extractJWT (req :Request): string|null{
-    if(req.cookies && "Token" in req.cookies){
-        return req.cookies.Token
+  private static extractJWT(req: Request): string | null {
+    if (req.headers.authorization) {
+      const [_, token] = req.headers.authorization.split(" ")
+      return token
     }
     return null;
   }
 
 
   // validate token 
-  async validate(payload :{id:string ;email:string}) {
+  async validate(payload: { id: string; email: string }) {
     return payload;
   }
 }
